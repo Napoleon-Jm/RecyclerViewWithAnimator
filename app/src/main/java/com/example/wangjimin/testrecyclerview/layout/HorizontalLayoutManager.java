96457cfb5b8e038ee1309eccc5c28aff83029ad3
恺心 <jimin.wjm@alipay.com>
2016-08-22 00:13:01 +0800

    解决Recycler低版本wrap_content属性measure宽度为0的bug

---
+++ app/src/main/java/com/example/wangjimin/testrecyclerview/layout/HorizontalLayoutManager.java
@@ -0,0 +1,111 @@
+package com.example.wangjimin.testrecyclerview.layout;
+
+import android.content.Context;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.antfortune.wealth.common.util.LogUtils;
+
+/**
+ * Created by wangjimin on 16/8/11.
+ *
+ * 配合BounceHorizontalView使用, RecyclerView 在23.0.3之前Layout测量有Bug,用此类解决.
+ */
+public class HorizontalLayoutManager extends LinearLayoutManager {
+
+    private static final String TAG = HorizontalLayoutManager.class.getSimpleName();
+    private int paddingLeft;
+    private int paddingRight;
+    private int itemDecorWidth;
+
+    public HorizontalLayoutManager(Context context, int paddingLeft, int paddingRight, int itemDecor) {
+        super(context);
+        itemDecorWidth = itemDecor;
+        this.paddingLeft = paddingLeft;
+        this.paddingRight = paddingRight;
+    }
+
+    public HorizontalLayoutManager(Context context, int orientation, boolean reverseLayout) {
+        super(context, orientation, reverseLayout);
+    }
+
+    private int[] mMeasuredDimension = new int[2];
+
+    @Override
+    public void onMeasure(RecyclerView.Recycler recycler, RecyclerView.State state,
+                          int widthSpec, int heightSpec) {
+
+        final int widthMode = View.MeasureSpec.getMode(widthSpec);
+        final int heightMode = View.MeasureSpec.getMode(heightSpec);
+        final int widthSize = View.MeasureSpec.getSize(widthSpec);
+        final int heightSize = View.MeasureSpec.getSize(heightSpec);
+
+        int width = 0;
+        int height = 0;
+        for (int i = 0; i < getItemCount(); i++) {
+            measureScrapChild(recycler, i,
+                    View.MeasureSpec.makeMeasureSpec(i, View.MeasureSpec.UNSPECIFIED),
+                    View.MeasureSpec.makeMeasureSpec(i, View.MeasureSpec.UNSPECIFIED),
+                    mMeasuredDimension);
+
+            if (getOrientation() == HORIZONTAL) {
+                width = width + mMeasuredDimension[0];
+                if (i == 0) {
+                    height = mMeasuredDimension[1];
+                }
+            } else {
+                height = height + mMeasuredDimension[1];
+                if (i == 0) {
+                    width = mMeasuredDimension[0];
+                }
+            }
+        }
+        //针对第一个孩子左边(paddingLeft)和最后一个孩子右边(paddingRight)都有itemDecor的情况,
+        //只针对横向布局,纵向布局有待完善
+        width += (getItemCount()-1)*itemDecorWidth;
+        width += (paddingLeft + paddingRight);
+        switch (widthMode) {
+            case View.MeasureSpec.EXACTLY:
+                width = widthSize;
+            case View.MeasureSpec.AT_MOST:
+            case View.MeasureSpec.UNSPECIFIED:
+        }
+
+        switch (heightMode) {
+            case View.MeasureSpec.EXACTLY:
+                height = heightSize;
+            case View.MeasureSpec.AT_MOST:
+            case View.MeasureSpec.UNSPECIFIED:
+        }
+
+        setMeasuredDimension(width, height);
+    }
+
+    private void measureScrapChild(RecyclerView.Recycler recycler, int position, int widthSpec,
+                                   int heightSpec, int[] measuredDimension) {
+        try {
+            View view = recycler.getViewForPosition(position);//fix 动态添加时报IndexOutOfBoundsException
+
+            if (view != null) {
+                RecyclerView.LayoutParams p = (RecyclerView.LayoutParams) view.getLayoutParams();
+
+                int childWidthSpec = ViewGroup.getChildMeasureSpec(widthSpec,
+                        getPaddingLeft() + getPaddingRight(), p.width);
+
+                int childHeightSpec = ViewGroup.getChildMeasureSpec(heightSpec,
+                        getPaddingTop() + getPaddingBottom(), p.height);
+
+                view.measure(childWidthSpec, childHeightSpec);
+                measuredDimension[0] = view.getMeasuredWidth() + p.leftMargin + p.rightMargin;
+                measuredDimension[1] = view.getMeasuredHeight() + p.bottomMargin + p.topMargin;
+                recycler.recycleView(view);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.d(TAG, e.getClass().getName());
+        }
+    }
+}
